cad78bd877cb104e4325bd1d53f0591c8017a08a
adilamanm01 <114213304+adilamanm01@users.noreply.github.com>
2022-11-14 15:39:28 -0600

    Create option2.c

---
+++ option2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int main()
+{
+    //declaration of char type variable
+   char str[100];
+   //asking for Reference number
+   printf("\nenter the Reference Number:\n");
+   
+      scanf("%[^\n]s",str);
+      printf("%s\n",str);
+      printf("\n");
+   
+   //seraching for Reference number and printing the details of specific item
+   
+   
+   
+   return 0;
+}
