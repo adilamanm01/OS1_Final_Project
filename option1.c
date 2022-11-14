e4126823f5c98bb4271396d311abd834e49ebd0f
adilamanm01 <114213304+adilamanm01@users.noreply.github.com>
2022-11-13 20:21:00 -0600

    Update option1.c

--- option1.c
+++ option1.c
@@ -1,45 +1,35 @@
+// C program to implement
+// the above approach
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <pthread.h>
 #include <string.h>
 
-int main(int arg_count, char *arg_values[])
+// Driver code
+int main()
 {
-	FILE *file_pointer ;
-	char file_line[1255],*line,*ref;
-    char jwell_details[5][1255];
-	file_pointer = fopen("./cartier_catalog.txt", "r") ;
-	if ( file_pointer == NULL )
-	{
-		printf("cartier_catalog.txt file failed to open." ) ;
-        exit;
-	}	
-	else
-	{
-		fgets ( file_line, 255, file_pointer );
-		while( fgets ( file_line, 1255, file_pointer )!= NULL )
-		{
-			char *str = strtok(file_line, "\t");
-			int c=0;
-            ref = arg_values[1];
-			if (strcmp(str, ref)==0){
-    			while (str != NULL)
-    			{
-        			strcpy(jwell_details[c],str);
-        			str = strtok(NULL, "\t");
-					c=c+1;
-    			}
-				fclose(file_pointer);
-			}
-		}
-	}
-    printf ("Jwell Details are :\n"  ) ;
-	printf ("Ref : %s\n" , jwell_details[0] ) ;
-	printf ("Category : %s\n" , jwell_details[1] ) ;
-	printf ("Title : %s\n" , jwell_details[2] ) ;
-	printf ("Tags : %s\n" , jwell_details[3] ) ; 
-	printf ("Price : %s\n" , jwell_details[6] ) ; 
-	
-}
+	FILE* ptr;
+	char ch;
 
+	// Opening file in reading mode
+	ptr = fopen("op1.txt", "r");
+
+	if (NULL == ptr) {
+		printf("file can't be opened \n");
+	}
+
+	//printf("content of this file are \n");
+
+	// Printing what is written in file
+	// character by character using loop.
+	do {
+		ch = fgetc(ptr);
+		printf("%c", ch);
+
+		// Checking if character is not EOF.
+		// If it is EOF stop eading.
+	} while (ch != EOF);
+
+	// Closing the file
+	fclose(ptr);
+	return 0;
+}
